36f33a02672436089310a55aa4af5e5ab9a6ad0c
CertSkill <startup.vittorio@gmail.com>
2025-05-22 15:12:32 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Certificazione Team Work")
+st.subheader("Benvenuto nel primo test per valutare il tuo spirito di collaborazione!")
+
+st.write("Prossimamente potrai interagire con un team virtuale per ottenere la tua certificazione.")
